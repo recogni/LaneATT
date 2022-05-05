6bde9c7a2600d926f80c614b5525f3351a133faa
Vincent Mayer <vincent.mayer@tum.de>
2022-05-05 15:19:28 +0000

    feat: saving of generated json

--- test_dataset.py
+++ test_dataset.py
@@ -12,8 +12,13 @@ def setup_logging():
 
 if __name__ == "__main__":
     setup_logging()
-    lds = LaneDataset(split='train', root="datasets/tusimple")
-    sample = lds[0]
-    sample_out = {"lanes": sample[2].tolist(), "h_samples": lds.offsets_ys.tolist(), "raw_file": sample[1]}
-    with open("label_laneatt.json", "w") as f:
-        json.dump(sample_out, f)
+    split = "test"
+    size = (720, 1280)
+    lds = LaneDataset(split=split, img_size=(720, 1280), root="datasets/tusimple" if split in ["train", "val"] else "datasets/tusimple-test")
+    f = open(f"processed_{split}_{size[0]}-{size[1]}_labels_laneatt.json", "w")
+    for i, sample in enumerate(lds):
+        sample_out = {"lanes": sample[2].tolist(), "h_samples": lds.offsets_ys.tolist(), "raw_file": sample[1]}
+        f.write(json.dumps(sample_out))
+        f.write("\n")
+    f.close()
+
