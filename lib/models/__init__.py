23dc9b4c09c1f60c41f808859c8de062b6addd27
Alok <alok@recogni.com>
2022-05-06 14:55:17 +0200

    remove old lane att code

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,5 +1,3 @@
 from .laneatt import LaneATT
-from .laneatt_old import LaneATT as LaneATT_old
-from .laneatt_no_att import LaneATT as LaneATT_no_att
 
-__all__ = ["LaneATT", "LaneATT_old", "LaneATT_no_att"]
+__all__ = ["LaneATT"]
